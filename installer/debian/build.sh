073b4d5d8415a20df9f84576706a2c93a917cab4
Georg Sieber <it@georg-sieber.de>
2023-07-02 17:16:37 +0200

    adjust debian installer

--- installer/debian/build.sh
+++ installer/debian/build.sh
@@ -12,16 +12,21 @@ fi
 # cd to working dir
 cd "$(dirname "$0")"
 
+# compile language files
+lrelease ../../lang/*.ts
+
 # empty / create necessary directories
 if [ -d "jabber4linux/usr/share/jabber4linux" ]; then
 	rm -r jabber4linux/usr/share/jabber4linux
 fi
+mkdir -p jabber4linux/usr/share/jabber4linux/lang
 mkdir -p jabber4linux/usr/share/jabber4linux/assets
 mkdir -p jabber4linux/usr/share/applications
 
 # copy files in place
 cp ../../assets/*.desktop jabber4linux/usr/share/applications
 cp ../../*.py jabber4linux/usr/share/jabber4linux
+cp ../../lang/*.qm jabber4linux/usr/share/jabber4linux/lang
 cp ../../assets/*.svg jabber4linux/usr/share/jabber4linux/assets
 cp ../../assets/*.wav jabber4linux/usr/share/jabber4linux/assets
 cp ../../README.md jabber4linux/usr/share/jabber4linux
