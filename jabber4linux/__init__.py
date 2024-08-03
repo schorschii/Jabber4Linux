5d7a7b6219a8d2188f40f76139f457935824a550
Georg Sieber <it@georg-sieber.de>
2024-08-03 21:04:57 +0200

    bump version

--- jabber4linux/__init__.py
+++ jabber4linux/__init__.py
@@ -1,7 +1,7 @@
 __title__   = 'Jabber4Linux'
 __author__  = 'Georg Sieber'
 __license__ = 'GPL-3.0'
-__version__ = '0.5.2'
+__version__ = '0.6.0'
 __website__ = 'https://github.com/schorschii/Jabber4Linux'
 
 __all__ = [__author__, __license__, __version__]
